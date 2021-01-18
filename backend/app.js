25ea8055d730cd83d755859d649016090776e1e4
thedoft <thedoft@gmail.com>
2021-01-18 21:08:00 +0300

    feat: add crash test

--- backend/app.js
+++ backend/app.js
@@ -30,6 +30,12 @@ app.use(requestLogger);
 
 app.use('*', cors(corsConfig));
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.post('/signup', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
